f1ea757f2249b2192d4fd92b6b1025d8e8126399
Nguyễn Chí Trung <trungtada123@gmail.com>
2025-03-14 17:10:18 +0700

    Fix

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\ptud-gk-de-1\flask-tiny-app\venv
+set VIRTUAL_ENV=D:\ThuNghiem\ptud-gk-de-1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
